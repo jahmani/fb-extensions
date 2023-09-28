906b776b0b6d26e9d5582117fac285f60879b058
jahmani <jahmani@gmail.com>
2023-09-28 12:45:31 +0300

    uploadTask: compress img and set img metadata

--- apps/products-gallery-app/src/app/ui-components/UploadTask/upload-task.component.ts
+++ apps/products-gallery-app/src/app/ui-components/UploadTask/upload-task.component.ts
@@ -24,10 +24,11 @@ import {
 } from '@store-app-repository/app-models';
 
 import { Storage } from '@angular/fire/storage';
-import { Subscription, tap, map } from 'rxjs';
+import { tap, map } from 'rxjs';
 import { ImageCropModalComponent } from '../ImageCropModal/image-crop-modal.component';
 import { storeIdToken } from '../../app.routes';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
+import imageCompression, { Options } from 'browser-image-compression';
 
 @Component({
   selector: 'store-app-repository-upload-task',
@@ -116,24 +117,24 @@ export class UploadTaskComponent implements OnInit, OnDestroy {
     });
   }
 
-  imgFileLoded(slideImgFile: HTMLImageElement) {
-    if (!this.imgInfo.imageMeta) {
-      const width = slideImgFile.naturalWidth;
-      const height = slideImgFile.naturalHeight;
-      console.log('width: ', width, 'height: ', height);
-      this.imgInfo.imageMeta = {
-        size: this.imgInfo.size,
-        width,
-        height,
-        subName: '',
-      };
+  // imgFileLoded(slideImgFile: HTMLImageElement) {
+  //   if (!this.imgInfo.imageMeta) {
+  //     const width = slideImgFile.naturalWidth;
+  //     const height = slideImgFile.naturalHeight;
+  //     console.log('width: ', width, 'height: ', height);
+  //     this.imgInfo.imageMeta = {
+  //       size: this.imgInfo.size,
+  //       width,
+  //       height,
+  //       subName: '',
+  //     };
 
-      const imgmetas: ImageMeta[] = this.getThumbsProperties(
-        this.imgInfo.imageMeta
-      );
-      this.imgInfo.thumbsMeta = imgmetas;
-    }
-  }
+  //     const imgmetas: ImageMeta[] = this.getThumbsProperties(
+  //       this.imgInfo.imageMeta
+  //     );
+  //     this.imgInfo.thumbsMeta = imgmetas;
+  //   }
+  // }
   private getThumbsProperties(imageMeta: ImageMeta) {
     const thumbSizes = [200, 600, 1500];
     const imgmetas: ImageMeta[] = [];
@@ -160,28 +161,60 @@ export class UploadTaskComponent implements OnInit, OnDestroy {
 
     // Reference to storage bucket
     const storageRef = ref(this.storage, path);
-    let meta = {};
-    if (this.imgInfo.cropInfo) {
-      const { width, height, ...rest } = this.imgInfo.cropInfo;
-      const size = this.imgInfo.cropInfo.blob?.size;
-      meta = {
-        size: size?.toString(),
-        width: width.toString(),
-        height: height.toString(),
-      };
-    } else {
-      const { width, height, size, ...rest } = this.imgInfo.imageMeta;
-      meta = {
-        size: size?.toString(),
-        width: width.toString(),
-        height: height.toString(),
-      };
-    } // The main task
-    const uploadTask = uploadBytesResumable(
-      storageRef,
-      this.imgInfo.cropInfo?.blob || this.imgInfo.file,
-      { ...meta, cacheControl: 'private,max-age=2592000' }
+    // let customMetadata = {};
+    // if (this.imgInfo.cropInfo) {
+    //   const { width, height, ...rest } = this.imgInfo.cropInfo;
+    //   const size = this.imgInfo.cropInfo.blob?.size;
+    //   customMetadata = {
+    //     size: size?.toString(),
+    //     width: width.toString(),
+    //     height: height.toString(),
+    //   };
+    // } else {
+    //   const { width, height, size, ...rest } = this.imgInfo.imageMeta;
+    //   customMetadata = {
+    //     size: size?.toString(),
+    //     width: width.toString(),
+    //     height: height.toString(),
+    //     thumbSizes: '200,600,1500',
+    //   };
+    // } // The main task
+
+    let file = this.imgInfo.file;
+    if (this.imgInfo.cropInfo?.blob) {
+      file = new File([this.imgInfo.cropInfo?.blob], this.imgInfo.name, {
+        type: this.imgInfo.type,
+      });
+    }
+    let compressedFile = await this.createCompressedFile(file);
+    compressedFile =
+      compressedFile && compressedFile.size < file.size ? compressedFile : file;
+
+    const { width, height } = await this.calculateImageDimentions(
+      compressedFile
     );
+    this.imgInfo.imageMeta = {
+      size: compressedFile.size,
+      width,
+      height,
+      subName: '',
+    };
+
+    const imgmetas: ImageMeta[] = this.getThumbsProperties(
+      this.imgInfo.imageMeta
+    );
+    this.imgInfo.thumbsMeta = imgmetas;
+
+    const customMetadata = {
+      size: compressedFile.size?.toString(),
+      width: width.toString(),
+      height: height.toString(),
+      thumbSizes: '200,600,1500',
+    };
+    const uploadTask = uploadBytesResumable(storageRef, compressedFile, {
+      customMetadata,
+      cacheControl: 'private,max-age=2592000',
+    });
     uploadTask.then(
       async (snapshot) => {
         //  const downloadUrl = await getDownloadURL(storageRef);
@@ -211,6 +244,54 @@ export class UploadTaskComponent implements OnInit, OnDestroy {
     );
 
     this.imgInfo.uploadTaskData.snapshot = fromTask(uploadTask);
+  }
+
+  private async createCompressedFile(file: File) {
+    const options: Options = {
+      maxSizeMB: 0.5,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
+    };
+
+    let compressedFile;
+    try {
+      compressedFile = await imageCompression(file, options);
+      console.log(
+        'compressedFile instanceof Blob',
+        compressedFile instanceof Blob
+      ); // true
+      console.log(
+        `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
+      ); // smaller than maxSizeMB
+    } catch (error) {
+      console.log(error);
+    }
+    return compressedFile;
+  }
+  calculateImageDimentions(file: File) {
+    const p = new Promise<{ width: number; height: number }>(
+      (resolve, reject) => {
+        const img = document.createElement('img', {});
+        const objectURL = URL.createObjectURL(file);
+
+        img.onload = function handleLoad() {
+          console.log(`Width: ${img.width}, Height: ${img.height}`);
+
+          if (img.width < 100 || img.height < 100) {
+            console.log("The image's width or height is less than 100px");
+          }
+
+          URL.revokeObjectURL(objectURL);
+          resolve({ width: img.width, height: img.naturalHeight });
+        };
+        img.onerror = (e) => reject(e);
+
+        img.src = objectURL;
+
+        // document.body.appendChild(img);
+      }
+    );
+    return p;
   }
 
   async showModal(): Promise<ImageCroppedEvent | null> {
